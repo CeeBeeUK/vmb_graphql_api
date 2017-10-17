cf4131f2cdb44c5501c1522762071e9d2daa8641
Colin Bruce <colinbruce@gmail.com>
2017-10-17 16:07:51 +0100

    Add belongs to to evidence_board

--- app/models/evidence_board.rb
+++ app/models/evidence_board.rb
@@ -1,2 +1,3 @@
 class EvidenceBoard < ActiveRecord::Base
+  belongs_to :event
 end
--- spec/models/evidence_board_spec.rb
+++ spec/models/evidence_board_spec.rb
@@ -4,4 +4,6 @@ describe EvidenceBoard, type: :model do
   subject { build :evidence_board }
 
   it { is_expected.to be_valid }
+
+  it { is_expected.to belong_to :event }
 end
