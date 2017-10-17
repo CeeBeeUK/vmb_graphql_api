1e3d61c8d535dae7b3e81ab692dae8d4cca8a0a8
Colin Bruce <colinbruce@gmail.com>
2017-10-17 13:57:23 +0100

    Add shoulda test
    Add single test to ensure shoulda-matchers working

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,7 +1,8 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe Event, type: :model do
-  subject { described_class }
+  subject { described_class.new }
 
   it { is_expected.to respond_to :name }
+  it { is_expected.to validate_presence_of(:name) }
 end
