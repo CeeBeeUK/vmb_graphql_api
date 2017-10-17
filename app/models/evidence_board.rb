3f4cd423a3b3862dc13ef740aa6656a772455734
Colin Bruce <colinbruce@gmail.com>
2017-10-17 16:47:18 +0100

    Extend evidence_board tests

--- app/models/evidence_board.rb
+++ app/models/evidence_board.rb
@@ -1,3 +1,6 @@
 class EvidenceBoard < ActiveRecord::Base
   belongs_to :event
+
+  validates :position, uniqueness: { scope: :event_id, case_sensitive: false }
+  validates :label, uniqueness: { scope: :event_id, case_sensitive: false }
 end
--- spec/models/evidence_board_spec.rb
+++ spec/models/evidence_board_spec.rb
@@ -6,4 +6,21 @@ describe EvidenceBoard, type: :model do
   it { is_expected.to be_valid }
 
   it { is_expected.to belong_to :event }
+
+  describe 'validation' do
+    it { is_expected.to validate_uniqueness_of(:position).ignoring_case_sensitivity.scoped_to(:event_id) }
+    it { is_expected.to validate_uniqueness_of(:label).ignoring_case_sensitivity.scoped_to(:event_id) }
+
+    context 'within a single event' do
+      subject(:scoped_board) { build :evidence_board, label: 'Board A', event: event_two }
+
+      let(:event_one) { create :event, name: 'Event One' }
+      let(:event_two) { create :event, name: 'Event Two' }
+      before do
+        create :evidence_board, label: 'Board A', event: event_one
+      end
+
+      it { is_expected.to be_valid }
+    end
+  end
 end
