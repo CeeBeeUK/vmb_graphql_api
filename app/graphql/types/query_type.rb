7ab9c5a8e341dbc1b832aa83ae3e8860eb2f970b
Colin Bruce <colinbruce@gmail.com>
2017-10-18 14:56:24 +0100

    Extend query
    Add singular event

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,9 +1,18 @@
 Types::QueryType = GraphQL::ObjectType.define do
-  name "Query"
-
+  name 'Query'
+  description 'Query root for this schema'
   field :events, !types[Types::EventType] do
     resolve ->(obj, args, ctx) {
       Event.all
+    }
+  end
+
+  field :event, !types[Types::EventType] do
+    description 'Return a single event'
+    argument :id, !types.ID
+    resolve ->(obj, args, ctx) {
+      # Event.where(id: args[:id]).first
+      Event.where(id: args[:id])
     }
   end
 
